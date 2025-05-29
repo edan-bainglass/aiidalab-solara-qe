8932ba15b9f89690bbf960e1277be50caab126c0
Edan Bainglass <edan.bainglass@gmail.com>
2025-05-29 08:57:06 +0200

    Cleanup

--- src/aiidalab_qe/pages/home/home.py
+++ src/aiidalab_qe/pages/home/home.py
@@ -24,7 +24,7 @@ def Home():
         NavBar(
             [
                 {
-                    "label": "Getting started",
+                    "label": "Get started",
                     "icon": "rocket",
                 },
                 {
--- src/aiidalab_qe/pages/workbench/__init__.py
+++ src/aiidalab_qe/pages/workbench/__init__.py
@@ -1,15 +1,5 @@
-import solara
-
 from .workbench import Workbench
 
 __all__ = [
     "Workbench",
 ]
-
-
-@solara.component
-def Layout(children: list[solara.Element]):
-    with solara.Sidebar():
-        with solara.v.ListItem():
-            solara.Text("<pk=93> Calculation 1")
-    solara.Div(children=children)
