ac69abb917a9932085aabe6e59f87539566f98f3
Edan Bainglass <edan.bainglass@gmail.com>
2025-07-02 10:02:12 +0000

    Add proxy server configuration

---
+++ src/aiidalab_qe/config/proxy.py
@@ -0,0 +1,34 @@
+import os
+
+
+def _app_command(port):
+    os.environ["SOLARA_APP"] = "src/aiidalab_qe/main.py"
+    os.environ["APP_PORT"] = str(port)
+    return [
+        "uvicorn",
+        "aiidalab_qe.api.main:app",
+        "--host",
+        "0.0.0.0",
+        "--port",
+        str(port),
+        "--reload",
+    ]
+
+
+def setup_app():
+    icon_path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "../assets/images",
+            "icon.svg",
+        )
+    )
+    return {
+        "command": _app_command,
+        "timeout": 60,
+        "absolute_url": False,
+        "launcher_entry": {
+            "title": "AiiDAlab Quantum ESPRESSO",
+            "icon_path": icon_path,
+        },
+    }
